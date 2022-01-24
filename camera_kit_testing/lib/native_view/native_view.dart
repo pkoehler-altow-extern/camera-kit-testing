50451591b270fb8182c53e5893c8e98fff0a031a
Paul Köhler <kontakt@paul-koehler.dev>
2022-01-24 08:45:28 +0100

    adding basic ios native render

--- camera_kit_testing/lib/native_view/native_view.dart
+++ camera_kit_testing/lib/native_view/native_view.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:camera_kit_testing/native_view/native_view_android.dart';
 import 'package:camera_kit_testing/native_view/native_view_ios.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class NativeView extends StatelessWidget {
@@ -9,14 +10,17 @@ class NativeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (Platform.isAndroid) {
-      return const NativeViewAndroid();
-    } else if (Platform.isIOS) {
-      return const NativeViewIos();
-    } else {
-      return const Center(
-        child: Text('UNIMPLEMENTED'),
-      );
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        // return widget on Android.
+        return const NativeViewAndroid();
+      case TargetPlatform.iOS:
+        // return widget on iOS.
+        return const NativeViewIos();
+      default:
+        return const Center(
+          child: Text('UNIMPLEMENTED'),
+        );
     }
   }
 }
--- camera_kit_testing/lib/native_view/native_view_ios.dart
+++ camera_kit_testing/lib/native_view/native_view_ios.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NativeViewIos extends StatelessWidget {
   const NativeViewIos({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // This is used in the platform side to register the view.
+    const String viewType = '<platform-view-type>';
+    // Pass parameters to the platform side.
+    final Map<String, dynamic> creationParams = <String, dynamic>{};
+
+    return UiKitView(
+      viewType: viewType,
+      layoutDirection: TextDirection.ltr,
+      creationParams: creationParams,
+      creationParamsCodec: const StandardMessageCodec(),
+    );
   }
 }
